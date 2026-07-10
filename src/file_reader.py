fbee3ed356e27c41c11dcecfb440f79067469868
FerGod_Dev <84854172+Fernando-Alvarado-Soria@users.noreply.github.com>
2026-07-10 14:02:07 -0600

    reacomodo de acceses

---
+++ src/file_reader.py
@@ -0,0 +1,12 @@
+#escribe aquí un código para la tarea
+# Abrir el archivo en modo lectura
+archivo = open("my_file.txt", "r")
+
+# Leer todo el contenido
+contenido = archivo.read()
+
+# Mostrar el contenido
+print(contenido)
+
+# Cerrar el archivo
+archivo.close()
